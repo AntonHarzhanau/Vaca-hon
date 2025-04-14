81b4afe470eccab2e050b4281bbf4b01caad25ac
FAYEZ KARIM-SHADY-MONIR <karim-shady-monir.fayez@etu.unistra.fr>
2025-04-14 16:56:39 +0200

    Pop-up offer and property card design 95% done

--- scripts/UI/Popups/popup_offre.gd
+++ scripts/UI/Popups/popup_offre.gd
@@ -10,7 +10,8 @@ func _ready():
 	deny_btn.pressed.connect(_on_deny_pressed)
 
 func show_offer(cell_name: String):
-	label_offer.text = "Do you want to buy " + cell_name + " for $" + str(price) + "?"
+	$Back_ground/Title_Label.text = cell_name
+	$Back_ground/Label.text = "Do you want to buy this property for $" + str(price) + "?"
 	visible = true
 	#TODO: add check if the player has enough money and hide accept_btn if there is not enough money
 
