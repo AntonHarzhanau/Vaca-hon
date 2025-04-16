2b3cc2e8696ca6d2433f70807764656104b57596
FAYEZ KARIM-SHADY-MONIR <karim-shady-monir.fayez@etu.unistra.fr>
2025-04-16 20:04:57 +0200

    Résolution des conflits entre 3.Advanced_Mechanics et 6.7_TingtingJiang

--- scripts/UI/Popups/popup_offre.gd
+++ scripts/UI/Popups/popup_offre.gd
@@ -10,12 +10,6 @@ func _ready():
 	deny_btn.pressed.connect(_on_deny_pressed)
 
 func show_offer(cell_name: String):
-<<<<<<< HEAD
-	$Back_ground/Title_Label.text = cell_name
-	$Back_ground/Label.text = "Do you want to buy this property for $" + str(price) + "?"
-=======
-	label_offer.text = "Do you want to buy " + cell_name + " for $" + str(price) + "?"
->>>>>>> 6.7_TingtingJiang
 	visible = true
 	#TODO: add check if the player has enough money and hide accept_btn if there is not enough money
 
