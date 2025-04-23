7556d85ca4bfead2dbfe1f80280b8b81aa50dbdb
RaoufBoba(1) <boubachiche>
2025-04-23 11:12:09 +0200

    ajout musique arriere-plan/effet

--- scripts/UI/ui.gd
+++ scripts/UI/ui.gd
@@ -1,17 +1,23 @@
 extends CanvasLayer 
-class_name UI
+
 signal end_turn_clicked
 signal move_player_requested(steps)
 signal accept_offer_clicked
 signal sell_property_clicked(cell_id)
 
-const GUEST_HUB = preload("res://scenes/UI/Hub/guest_hub.tscn")
-@onready var dice:Dice = $Dice
-@onready var main_player_hub:MainPlayerHub = $MainPlayerHub
+@onready var dice = $Dice
+@onready var hub = $Hub
 @onready var popup_offer = $PopUpOffre
-@onready var end_turn_btn: Button = $EndTurnButton
-@onready var turn_lable:Label = $TurnLable
-@onready var guest_hubs_container:VBoxContainer = $GuestHubContainer
+@onready var end_turn_btn = $EndTurnButton
+@onready var turn_lable = $TurnLable
+@onready var quit_dialog = $QuitDialog
+@onready var help_popup = $HelpPopup
+@onready var name_edit = $HelpPopup/VBoxContainer/NameEdit
+@onready var phone_edit = $HelpPopup/VBoxContainer/PhoneEdit
+@onready var email_edit = $HelpPopup/VBoxContainer/EmailEdit
+@onready var message_edit = $HelpPopup/VBoxContainer/MessageEdit
+@onready var send_button = $HelpPopup/VBoxContainer/SendButton
+@onready var help_button = $HelpButton
 
 var id_player_on_turn: int = 0
 
@@ -20,47 +26,80 @@ func _ready() -> void:
 	turn_lable.text = "Player's turn: 0"
 	dice.dice_rolled.connect(_on_dice_dice_rolled)
 	end_turn_btn.pressed.connect(_on_end_turn_button_pressed)
-	
+	quit_dialog.confirmed.connect(_on_quit_dialog_confirmed)
+	help_popup.hide()
+	help_button.pressed.connect(_on_help_button_pressed)
+	send_button.pressed.connect(_on_sned_button_pressed)
+
 func _on_dice_dice_rolled(d1: int, d2: int):
-	#TODO: change logic
 	var msg = {"action": "move_player", "steps": d1 + d2}
 	WebSocketClient.send_message(JSON.stringify(msg))
 
-func create_main_player_hub(player:Player):
-	main_player_hub.set_player(player)
-	main_player_hub.update_hub()
+# Handling signals from GameManager
 
-func create_guest_hub(player:Player):
-	var guest_hub = GUEST_HUB.instantiate()
-	guest_hub.player_id = player.id
-	guest_hub.set_player(player)
-	guest_hubs_container.add_child(guest_hub)
-	guest_hub.update_hub()
-
-func update_hubs(player:Player, current_player_id):
-	print("hear")
-	if player.id == current_player_id:
-		main_player_hub.update_hub()
-	else:
-		for hub in guest_hubs_container.get_children():
-			if player.id == hub.player_id:
-				hub.update_hub()
+func update_hub(money: int, properties: Array):
+	hub.set_player_money(money)
+	hub.update_properties_list(properties)
 
 func _on_player_disconnected(player_id):
-	for hub in guest_hubs_container.get_children():
-		if player_id == hub.player_id:
-			hub.queue_free()
+	# Remove from local collection, remove from UI
+	pass
 
 func _on_change_turn(player_id:int):
 	id_player_on_turn = player_id
 	turn_lable.text = "Player's turn: " + str(id_player_on_turn)
 
-func _on_offer_to_buy(_cell_id, cell_name, price):
-	popup_offer.show_offer(cell_name, price)
+func _on_offer_to_buy(cell_id, cell_name, price):
+		popup_offer.show_offer(cell_name, price)
+
+func _on_buy_property(money:int, properties:Array[Cell]):
+	update_hub(money, properties)
+	#TODO: add card to player or cell
 
 func _on_end_turn_button_pressed():
 	var msg = {"action": "end_turn"}
 	WebSocketClient.send_message(JSON.stringify(msg))
 
+
+func _on_sell_button_pressed(cell_id:int):
+	emit_signal("sell_property_clicked", cell_id)	
+
 func _on_pop_up_offre_accept_offer_clicked() -> void:
 	emit_signal("accept_offer_clicked")
+
+
+func _on_quitter_la_partie_pressed() :
+	quit_dialog.popup_centered() 
+	
+
+
+func _on_timer_timeout() -> void:
+	print("Temps écoulé ! Fin de la partie.")
+	quit_dialog.popup_centered() 
+
+
+func _on_reglages_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/reglages.tscn")
+
+
+func _on_quit_dialog_confirmed() -> void:
+	get_tree().quit()
+
+
+func _on_help_button_pressed():
+	help_popup.show()  
+
+func _on_sned_button_pressed():
+	var name = name_edit.text
+	var phone = phone_edit.text
+	var email = email_edit.text
+	var message = message_edit.text
+
+	if name.is_empty() or email.is_empty() or message.is_empty():
+		print("Veuillez remplir tous les champs obligatoires.")
+		return
+	
+	print("Nom:", name)
+	print("Téléphone:", phone)
+	print("Email:", email)
+	print("Message:", message)
--- scripts/reglages.gd
+++ scripts/reglages.gd
@@ -7,7 +7,7 @@ extends Node2D
 @onready var language_option = $Language
 @onready var privacy_dialog = $Privacy_Policy
 @onready var settings_manager = get_node("Settingsm") 
-
+@onready var sfx_click = $SFX_click
 func _ready():
 	settings_manager.load_settings()
 	
@@ -28,27 +28,34 @@ func _on_sfx_changed(value: float) -> void:
 func _on_vibrations_pressed() -> void:
 	settings_manager.vibration_enabled = vibration_toggle.button_pressed
 	settings_manager.save_settings()
+	sfx_click.play()
 
 func _on_notifications_pressed() -> void:
 	settings_manager.notifications_enabled = notification_toggle.button_pressed
 	settings_manager.save_settings()
+	sfx_click.play()
 
 func _on_language_option_item_selected(index: int):
 	settings_manager.language = index
 	settings_manager.save_settings()
+	sfx_click.play()
 
 
 func _on_privacy_policy_pressed():
 	privacy_dialog.popup_centered()
+	sfx_click.play()
 
 func _on_privacy_dialog_confirmed():
 	OS.shell_open("https://www.lelien.com/privacy")
 
 func _on_privacy_policy_2_pressed() -> void:
 	privacy_dialog.popup_centered()
+	sfx_click.play()
 
 func _on_datapp_pressed() -> void:
 	privacy_dialog.popup_centered()
+	sfx_click.play()
 
 func _on_review_data_consent_pressed() -> void:
 	privacy_dialog.popup_centered()
+	sfx_click.play()
